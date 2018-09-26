a37ff91aab52407c64d7e32bd58c1d865e353f25
zoey <echohyt@sohu.com>
2018-09-26 23:23:17 +0800

    "demo 1.1"

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById('canvas')
+var eraser = document.getElementById('eraser')
 var context = canvas.getContext('2d')
 autoSetCanvasSize(canvas)
-
+listenToMouse(canvas)
 
 //设置canvas的宽高与页面一致
 function autoSetCanvasSize(canvas){
@@ -18,6 +19,11 @@ function autoSetCanvasSize(canvas){
 }
 
 
+//橡皮擦
+var eraserEnabled = false
+eraser.onclick = function(){
+    eraserEnabled = true
+}
 
 
 //画线
@@ -30,22 +36,29 @@ function drawLine(x1,y1,x2,y2,color = 'black',lineWidth = 5){
     context.stroke();
     context.closePath()
 }
+
+//监听鼠标事件
 function listenToMouse(){
     var using = false
     let lastPoint = {x:undefined,y:undefined}
     canvas.onmousedown = function(a){
+        let x = a.clientX
+        let y = a.clientY
         using = true
-        if(using){
-            let x = a.clientX
-            let y = a.clientY
+        if(eraserEnabled){
+            context.clearRect(x-5,y-5,10,10)
+        }else{
             lastPoint = {x:x,y:y}
         }
         
     }
     canvas.onmousemove = function(a){
-        if(using){
-            let x = a.clientX
-            let y = a.clientY
+        let x = a.clientX
+        let y = a.clientY
+        if(!using){return}
+        if(eraserEnabled){
+            context.clearRect(x-5,y-5,10,10)
+        }else{      
             let newPoint = {x:x,y:y}
             drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
             lastPoint = newPoint
@@ -54,6 +67,5 @@ function listenToMouse(){
     }
     canvas.onmouseup = function(){
         using = false
-}
-
+    }
 }
