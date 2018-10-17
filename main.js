413a3827005925c6ae80d8c4cc7fe23f72e99ed5
zoey <echohyt@sohu.com>
2018-10-17 22:43:14 +0800

    增加了画笔颜色和清空、下载按钮

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function autoSetCanvasSize(canvas){
 }
 
 
-//橡皮擦
+//切换笔和橡皮擦
 var eraserEnabled = false
 eraser.onclick = function(){
     eraserEnabled = true
@@ -31,17 +31,49 @@ pen.onclick = function(){
     eraserEnabled = false
     pen.classList.add('active')
     eraser.classList.remove('active')
+    context.strokeStyle = 'black'
 }
 
-
+red.onclick = function(){
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    context.strokeStyle = 'red';
+}
+green.onclick = function(){
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+    context.strokeStyle = 'green'
+}
+blue.onclick = function(){
+    blue.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+    context.strokeStyle = 'blue'
+}
+clear.onclick = function(){
+    context.clearRect(0,0,canvas.width,canvas.height)
+}
+download.onclick = function(){
+    var url = canvas.toDataURL('image/png')
+    var a  = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    var name = window.prompt('给你的画起个名吧！') || '新建图片'
+    a.download = name
+    a.click()
+}
 
 //画线
-function drawLine(x1,y1,x2,y2,color = 'black',lineWidth = 5){
+function drawLine(x1,y1,x2,y2,color,lineWidth = 5){
     context.beginPath();
     context.strokeStyle = color;
+    context.fillStyle = color;
     context.moveTo(x1-10,y1-10);//起点
     context.lineWidth = lineWidth;
     context.lineTo(x2-10,y2-10);//终点
+    context.fill();
     context.stroke();
     context.closePath()
 }
@@ -78,7 +110,6 @@ function listenToMouse(){
         }
     }else{
         canvas.onmousedown = function(a){
-            console.log(a)
             let x = a.clientX
             let y = a.clientY
             using = true
